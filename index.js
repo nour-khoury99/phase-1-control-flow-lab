c99aa524d61565d5930ef2d935a3c45688fdaac2
nour khoury <khourynour79@gmail.com>
2022-09-07 12:36:25 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberFeet){
+
+
+  if(numberFeet <= 400){
+    return `This is one me! `;
+
+  }else if (numberFeet > 400){
+
+    return `no free sample`;
+
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+ 
+  return City === `Chicago`?`You are in the destination`:`This is another destination ${City}`;
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let genorosity;
+
+function switchOnCharmFromTip(num){
+
+  
+  switch(num){
+    case 'Coffee' :
+      genorosity = 'good ';
+      break;
+
+    case 'fruits' :
+      genorosity = 'Very Good';
+      break;
+
+    case 'sweets' :
+      genorosity = 'Want to stay here';
+      break;
+
+    default:
+      console.log("This city did not receive me as it should!");
+      break
+
+  }
 }
